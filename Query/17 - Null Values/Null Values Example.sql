3ef5ab3a64ca9439f5da8c12835b5be93b14b69a
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-07-31 14:44:18 +0330

    Null Values Example has been Added.

---
+++ Query/17 - Null Values/Null Values Example.sql
@@ -0,0 +1,7 @@
+-- IS NULL Checks for Null Values
+SELECT * FROM products
+WHERE Unit IS NULL;
+
+-- IS NOT NULL Checks for The values that are not Null
+SELECT * FROM products
+WHERE Unit IS NOT NULL;
