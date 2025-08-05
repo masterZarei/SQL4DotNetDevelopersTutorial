066b80e60c5429223d82fda95550bc76ff205e7f
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-05 17:35:11 +0330

    Update Subqueries (Scalar) Example.sql

--- Query/26 - Subqueries (Scalar)/Subqueries (Scalar) Example.sql
+++ Query/26 - Subqueries (Scalar)/Subqueries (Scalar) Example.sql
@@ -1,6 +1,19 @@
---A. Scalar Subquery
--- Find all products more expensive than the average price
-SELECT ProductName, Price
-FROM Products
+-- SCALAR SUBQUERIES
+-- 1. Get the average price of all products.
+SELECT AVG(Price) FROM Products;
+
+-- 2. Get the products with a price higher than the average price.
+SELECT * FROM Products
 WHERE Price > (SELECT AVG(Price) FROM Products);
 
+-- 3. Get the product with the highest price.
+SELECT * FROM Products
+WHERE Price = (SELECT MAX(Price) FROM Products);
+
+-- 4. Get the number of orders placed by the customer with ID = 5.
+SELECT COUNT(*) FROM Orders
+WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE CustomerID = 5);
+
+-- 5. Get the price of the product named 'Chang'.
+SELECT Price FROM Products
+WHERE ProductID = (SELECT ProductID FROM Products WHERE ProductName = 'Chang');
