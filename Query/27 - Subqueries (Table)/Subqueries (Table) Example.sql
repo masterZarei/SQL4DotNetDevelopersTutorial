673047a15cab5e2b098ad0ca75f4c1c60a949ebd
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-05 17:56:52 +0330

    Update Subqueries (Table) Example.sql

--- Query/27 - Subqueries (Table)/Subqueries (Table) Example.sql
+++ Query/27 - Subqueries (Table)/Subqueries (Table) Example.sql
@@ -1,9 +1,35 @@
---C. Table Subquery
--- Average price of products per category using a derived table
-SELECT CategoryID, AvgPrice
-FROM (
-  SELECT CategoryID, AVG(Price) AS AvgPrice
-  FROM Products
-  GROUP BY CategoryID
-) AS CategoryAvg;
+--Table Subquery
+-- 1. Get all products supplied by suppliers from 'Germany'.
+SELECT * FROM Products
+WHERE SupplierID IN
+(SELECT SupplierID FROM Suppliers WHERE Country = 'Germany');
 
+-- 2. Get all orders placed by customers from 'Brazil'.
+SELECT * FROM Orders
+WHERE CustomerID IN
+(SELECT CustomerID FROM Customers WHERE Country = 'Brazil');
+
+-- 3. Get all products that are in the same category as the most expensive products
+SELECT *
+FROM products
+WHERE CategoryID IN (
+    SELECT CategoryID
+    FROM products
+    WHERE Price = (SELECT MAX(Price) FROM products)
+);
+
+-- 4. Get all customers who have placed at least one order
+SELECT *
+FROM customers
+WHERE CustomerID IN (
+    SELECT DISTINCT CustomerID
+    FROM orders
+);
+
+-- 5. Get products that have never been ordered
+SELECT *
+FROM products
+WHERE ProductID NOT IN (
+    SELECT DISTINCT ProductID
+    FROM order_details
+);
