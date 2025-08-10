dec9d1dc0c4da7211181701ab293cae55580d17f
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-11 00:15:33 +0330

    Creating Join Assets.

--- Query/31 - JOINs/JOINs.sql
+++ Query/31 - JOINs/JOINs.sql
@@ -1,11 +1,11 @@
---A JOIN clause is used to combine rows from two or more tables, based on a related column between them.
-
---(INNER) JOIN: Returns records that have matching values in both tables
+﻿--(INNER) JOIN: Returns records that have matching values in both tables
 --LEFT (OUTER) JOIN: Returns all records from the left table, and the matched records from the right table
 --RIGHT (OUTER) JOIN: Returns all records from the right table, and the matched records from the left table
 --FULL (OUTER) JOIN: Returns all records when there is a match in either left or right table
-SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
-FROM Orders
-INNER JOIN Customers
-ON Orders.CustomerID=Customers.CustomerID;
 
+-- نمایش نام مشتری و مجموع تعداد محصولاتی که سفارش داده
+SELECT Customers.CustomerName, SUM(order_details.Quantity) AS TotalQuantity
+FROM Customers
+INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+INNER JOIN order_details ON Orders.OrderID = order_details.OrderID
+GROUP BY Customers.CustomerName;
