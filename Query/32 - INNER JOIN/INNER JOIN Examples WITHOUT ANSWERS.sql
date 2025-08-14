4b6f225b293cc2f98d5d442f8a5146de7a026adc
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-15 01:02:33 +0330

    Create INNER JOIN Examples WITHOUT ANSWERS.sql

---
+++ Query/32 - INNER JOIN/INNER JOIN Examples WITHOUT ANSWERS.sql
@@ -0,0 +1,9 @@
+-- 1. Show customers and their orders
+
+-- 2. Show products that have been sold along with their quantities
+
+-- 3. Show orders along with the employee who created them
+
+-- 4. Show German customers and their orders
+
+-- 5. Show total quantity sold for each product
