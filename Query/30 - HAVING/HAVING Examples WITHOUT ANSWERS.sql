ab0704fab371584447aa7f8a1ae25ef5c7e217af
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-15 00:33:00 +0330

    Create HAVING Examples WITHOUT ANSWERS.sql

---
+++ Query/30 - HAVING/HAVING Examples WITHOUT ANSWERS.sql
@@ -0,0 +1,7 @@
+-- Show categories where the average product price is more than 50
+
+-- Show customers with more than 5 total orders
+
+-- Show products whose total sold quantity is under 100
+
+-- Show categories that have exactly 10 products
