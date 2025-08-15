d30d5c76ebb4c69200c008298ab80b852a7c80ab
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-16 00:35:21 +0330

    Create FULL OUTER JOIN Examples WITHOUT ANSWERS.sql

---
+++ Query/35 - FULL OUTER JOIN/FULL OUTER JOIN Examples WITHOUT ANSWERS.sql
@@ -0,0 +1,9 @@
+-- 1. Show all customers and all orders, even if they are unrelated
+
+-- 2. Show all products and all orders related to them
+
+-- 3. Show all employees and the orders they created
+
+-- 4. Show all countries and orders from customers in those countries
+
+-- 5. Show all customers and their total number of orders (NULL if unrelated)
