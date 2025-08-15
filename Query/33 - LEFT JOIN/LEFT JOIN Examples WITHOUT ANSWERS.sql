00a2915b44a4bc138c3deaead45d74c55210c78c
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-15 23:14:32 +0330

    Create LEFT JOIN Examples WITHOUT ANSWERS.sql

---
+++ Query/33 - LEFT JOIN/LEFT JOIN Examples WITHOUT ANSWERS.sql
@@ -0,0 +1,9 @@
+-- 1. Show all customers, even if they have no orders
+
+-- 2. Show all products, even if they have never been sold
+
+-- 3. Show all employees and the orders they created
+
+-- 4. Show customers and their order dates (NULL for those without orders)
+
+-- 5. Show all customers with the total number of orders (0 or NULL for no orders)
