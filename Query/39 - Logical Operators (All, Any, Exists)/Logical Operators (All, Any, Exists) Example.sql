f3e468f20db613f24597b45f5ca5584747a120d9
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-20 01:05:23 +0330

    Update Logical Operators (All, Any, Exists) Example.sql

--- Query/39 - Logical Operators (All, Any, Exists)/Logical Operators (All, Any, Exists) Example.sql
+++ Query/39 - Logical Operators (All, Any, Exists)/Logical Operators (All, Any, Exists) Example.sql
@@ -1,16 +1,12 @@
-﻿-- Find products that are cheaper than ANY product supplied by supplier 1
-SELECT ProductName, Price
-FROM Products
-WHERE Price < ANY (SELECT Price FROM Products WHERE SupplierID = 1);
---اگر یه محصول از حداقل یکی از محصولات تأمین‌کننده 1 ارزون‌تر باشه → انتخاب میشه.
-
--- Find products that are cheaper than ALL products supplied by supplier 1
-SELECT ProductName, Price
-FROM Products
-WHERE Price < ALL (SELECT Price FROM Products WHERE SupplierID = 1);
--- فقط محصولاتی انتخاب میشن که از همه‌ی محصولات تأمین‌کننده 1 ارزون‌تر باشن → یعنی ارزون‌ترین ارزون‌ها!
-
--- Find customers who have placed at least one order
-SELECT CustomerName
-FROM Customers
-WHERE EXISTS (SELECT * FROM Orders WHERE Orders.CustomerID = Customers.CustomerID);
+-- Any
+SELECT products.ProductName, PRICE
+FROM products
+WHERE PRICE < ANY (SELECT Price FROM products WHERE CategoryID = 1)
+-- All
+SELECT products.ProductName, PRICE
+FROM products
+WHERE PRICE < ALL (SELECT Price FROM products WHERE CategoryID = 1)
+-- Exists
+SELECT customers.CustomerName
+FROM customers
+WHERE EXISTS (SELECT * FROM orders WHERE orders.CustomerID = customers.CustomerID)
