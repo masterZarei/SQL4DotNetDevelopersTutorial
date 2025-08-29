c04c7a2cb89ccd560cc76c188c1de43823c3f242
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-30 00:37:16 +0330

    Update 44 - ISNULL and COALESCE Example.sql

--- Query/44 - ISNULL and COALESCE/44 - ISNULL and COALESCE Example.sql
+++ Query/44 - ISNULL and COALESCE/44 - ISNULL and COALESCE Example.sql
@@ -1,15 +1,6 @@
--- Show customers with PostalCode, or 'Unknown' if NULL
-SELECT CustomerName,
-       ISNULL(PostalCode, 'Unknown') AS PostalCode
-FROM Customers;
-
--- Show total quantity, if no items then 0
-SELECT ProductID,
-       ISNULL(SUM(Quantity), 0) AS TotalQuantity
-FROM Order_Details
-GROUP BY ProductID;
-
--- Show customers's living region (Country, else City, else 'Unknown')
 SELECT ContactName,
-       COALESCE(Country, City, 'Unknown') AS Location
-FROM customers;
+	ISNULL(PostalCode,'No Data') AS 'Postal Code'
+FROM customers
+
+SELECT ContactName, COALESCE(Country, City, 'Unknown')
+From Customers
