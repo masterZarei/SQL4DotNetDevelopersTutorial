fc7e551d4df36df38e3704d0d396f5ae87465416
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-08-30 00:55:16 +0330

    Update SELECT Case Example.sql

--- Query/45 - SELECT Case/SELECT Case Example.sql
+++ Query/45 - SELECT Case/SELECT Case Example.sql
@@ -1,25 +1,25 @@
 -- Categorize products as Cheap, Medium, or Expensive based on Price
 SELECT ProductName, Price,
-    CASE 
-        WHEN Price < 10 THEN 'Cheap'
-        WHEN Price BETWEEN 10 AND 30 THEN 'Medium'
-        ELSE 'Expensive'
-    END AS PriceCategory
-FROM Products;
+	CASE
+		WHEN Price < 10 THEN 'Cheap'
+		WHEN Price BETWEEN 10 AND 30 THEN 'Medium'
+		ELSE 'Expensive'
+	END AS PriceCategory
+FROM products
 
 -- Assign a Continent to each customer
-SELECT CustomerName, Country,
-    CASE 
-        WHEN Country IN ('Germany', 'UK', 'France') THEN 'Europe'
-        WHEN Country IN ('USA', 'Canada') THEN 'North America'
-        ELSE 'Other'
-    END AS Continent
-FROM Customers;
+SELECT ContactName, Country,
+	CASE
+		WHEN Country IN ('Germany','UK','France') THEN 'Europe'
+		WHEN Country IN ('USA','Canada','Mexico') THEN 'America'
+		ELSE 'Others'
+	END AS Continent
+FROM customers
 
 -- Label customers as Local or International
-SELECT CustomerName, Country,
-    CASE 
-        WHEN Country = 'USA' THEN 'Local'
-        ELSE 'International'
-    END AS CustomerType
-FROM Customers;
+SELECT ContactName, Country,
+	CASE
+		WHEN Country = 'USA' THEN 'Local'
+		ELSE 'International'
+	END AS 'Customer Type'
+FROM customers
