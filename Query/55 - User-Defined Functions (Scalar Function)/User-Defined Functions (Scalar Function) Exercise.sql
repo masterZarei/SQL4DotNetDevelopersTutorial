080f3bc27a02620bcae233f6208bbe4b54096533
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-10-19 09:10:55 +0330

    Update User-Defined Functions (Scalar Function) Exercise.sql

--- Query/55 - User-Defined Functions (Scalar Function)/User-Defined Functions (Scalar Function) Exercise.sql
+++ Query/55 - User-Defined Functions (Scalar Function)/User-Defined Functions (Scalar Function) Exercise.sql
@@ -3,44 +3,39 @@ CREATE FUNCTION fn_GetEmployeeAge (@BirthDate DATE)
 RETURNS INT
 AS
 BEGIN
-    RETURN DATEDIFF(YEAR, @BirthDate, GETDATE());
-END;
-
+	RETURN DATEDIFF(YEAR, @BirthDate, GETDATE())
+END
 -- Usage:
-SELECT FirstName, LastName, dbo.fn_GetEmployeeAge(BirthDate) AS Age
-FROM Employees;
-
+SELECT FirstName, LastName, dbo.fn_GetEmployeeAge(BirthDate)
+FROM employees
 
 
 --Concatenates first and last name into a full name.
-CREATE FUNCTION fn_GetFullName (@FirstName NVARCHAR(50), @LastName NVARCHAR(50))
-RETURNS NVARCHAR(100)
+CREATE FUNCTION fn_GetFullName(@firstName NVARCHAR(255), @lastName NVARCHAR(255))
+RETURNS NVARCHAR(255)
 AS
 BEGIN
-    RETURN @FirstName + ' ' + @LastName;
-END;
-
-
+	RETURN @firstName + ' ' + @lastName
+END
 -- Usage:
 SELECT dbo.fn_GetFullName(FirstName, LastName) AS FullName
-FROM Employees;
+From employees
+
 
 
 --Returns total value of a specific order.
-CREATE FUNCTION fn_GetOrderTotal (@OrderID INT)
-RETURNS DECIMAL(18,2)
+CREATE FUNCTION fn_GetOrderTotal(@OrderId INT)
+RETURNS float
 AS
 BEGIN
-    DECLARE @Total DECIMAL(18,2);
+	DECLARE @Total float
+	SELECT @Total = SUM(Quantity * p.price)
+	FROM order_details od
+	JOIN products p ON p.ProductID = od.ProductID
+	WHERE od.OrderID = @OrderId
 
-    SELECT @Total = SUM(Quantity * p.Price)
-    FROM Order_Details od
-    JOIN Products p ON od.ProductID = p.ProductID
-    WHERE od.OrderID = @OrderID;
-
-    RETURN ISNULL(@Total, 0);
-END;
-
+	RETURN ISNULL(@Total,0);
+END
 -- Usage:
-SELECT OrderID, dbo.fn_GetOrderTotal(OrderID) AS TotalAmount
-FROM Orders;
+SELECT OrderID, dbo.fn_GetOrderTotal(OrderId) AS TotalPrice
+FROM orders
