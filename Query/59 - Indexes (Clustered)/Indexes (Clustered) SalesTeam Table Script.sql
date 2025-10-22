c2fadf71db69a9b6fbc712949f487976ac57ee8b
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-10-22 11:07:56 +0330

    Create Indexes (Clustered) SalesTeam Table Script.sql

---
+++ Query/59 - Indexes (Clustered)/Indexes (Clustered) SalesTeam Table Script.sql
@@ -0,0 +1,10 @@
+﻿-- برای ایجاد جدول تیم فروش این اسکریپت را اجرا کنید
+USE [TopShopDb]
+GO
+CREATE TABLE [dbo].[SalesTeam](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[LastName] [nvarchar](50) NULL,
+	[BirthDate] [nvarchar](50) NULL,
+	[NationalCode] [nvarchar](10) NULL,
+)
