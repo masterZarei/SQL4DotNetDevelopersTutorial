0f96efc22cfb81a2d9ed240aa7c765dbdcdac9d2
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-11-14 08:34:13 +0330

    Update Data Encription (Column Encryption) Exercise.sql

--- Query/71 - Data Encription (Column Encryption)/Data Encription (Column Encryption) Exercise.sql
+++ Query/71 - Data Encription (Column Encryption)/Data Encription (Column Encryption) Exercise.sql
@@ -1,56 +1,26 @@
--------------------------------------------------------
--- STEP 1: Create master key (root for all encryption)
--------------------------------------------------------
-CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'MyStrongPassword@2025';
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'MyStrongPassword@2025'
 
-
--------------------------------------------------------
--- STEP 2: Create certificate to protect symmetric key
--------------------------------------------------------
 CREATE CERTIFICATE CustomerDataCert
-WITH SUBJECT = 'Encrypt customer personal info';
+WITH SUBJECT = 'Encrypt customer data'
 
-
--------------------------------------------------------
--- STEP 3: Create symmetric key with AES_256 algorithm
--------------------------------------------------------
 CREATE SYMMETRIC KEY CustomerSymKey
 WITH ALGORITHM = AES_256
 ENCRYPTION BY CERTIFICATE CustomerDataCert;
 
-
--------------------------------------------------------
--- STEP 4: Add a column to store encrypted data
--------------------------------------------------------
 ALTER TABLE Customers
-ADD EncryptedAddress VARBINARY(MAX);
+ADD EncryptedCountry VARBINARY(MAX)
+
+OPEN SYMMETRIC KEY CustomerSymKey DECRYPTION BY CERTIFICATE CustomerDataCert
+
+UPDATE customers
+SET EncryptedCountry = ENCRYPTBYKEY(KEY_GUID('CustomerSymKey'),Country)
+
+CLOSE SYMMETRIC KEY CustomerSymKey
 
 
--------------------------------------------------------
--- STEP 5: Encrypt existing Address data into new column
--------------------------------------------------------
-OPEN SYMMETRIC KEY CustomerSymKey DECRYPTION BY CERTIFICATE CustomerDataCert;
+OPEN SYMMETRIC KEY CustomerSymKey DECRYPTION BY CERTIFICATE CustomerDataCert
 
-UPDATE Customers
-SET EncryptedAddress = ENCRYPTBYKEY(KEY_GUID('CustomerSymKey'), Address);
+SELECT CustomerId, ContactName, CONVERT(VARCHAR(255), DECRYPTBYKEY(EncryptedCountry)) AS DecryptedCountry
+FROM customers
 
-CLOSE SYMMETRIC KEY CustomerSymKey;
-
-
--------------------------------------------------------
--- STEP 6: (Optional but recommended) Remove plain column
--------------------------------------------------------
-ALTER TABLE Customers DROP COLUMN Address;
-
-
--------------------------------------------------------
--- STEP 7: Decrypt data when reading
--------------------------------------------------------
-OPEN SYMMETRIC KEY CustomerSymKey DECRYPTION BY CERTIFICATE CustomerDataCert;
-
-SELECT 
-    CustomerID,
-    CONVERT(NVARCHAR(100), DECRYPTBYKEY(EncryptedAddress)) AS DecryptedAddress
-FROM Customers;
-
-CLOSE SYMMETRIC KEY CustomerSymKey;
+CLOSE SYMMETRIC KEY CustomerSymKey
