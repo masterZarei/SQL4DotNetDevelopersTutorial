112f77e8a1303d45a4938cbb609e36500f232ecb
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-11-16 13:07:47 +0330

    Update DigitalLibrary (Creating Tables).sql

--- Query/73 - DigitalLibrary (Creating Tables)/DigitalLibrary (Creating Tables).sql
+++ Query/73 - DigitalLibrary (Creating Tables)/DigitalLibrary (Creating Tables).sql
@@ -4,15 +4,15 @@ USE DigitalLibraryDb
 
 CREATE TABLE Category(
 	Id INT PRIMARY KEY Identity,
-	Title NVARCHAR(255)
+	Title NVARCHAR(255) Not Null
 )
 Create TABLE [Role](
 	Id INT PRIMARY KEY Identity,
-	Title NVARCHAR(255)
+	Title NVARCHAR(255) Not Null
 )
 CREATE TABLE Book(
 	Id INT PRIMARY KEY Identity,
-	Title NVARCHAR(255),
+	Title NVARCHAR(255) Not Null,
 	[Description] NVARCHAR(MAX),
 	ImageUrl NVARCHAR(MAX),
 	FileUrl NVARCHAR(MAX),
@@ -20,16 +20,19 @@ CREATE TABLE Book(
 	CategoryId INT,
 	IsDeleted bit,
 	CONSTRAINT FK_Book_Category FOREIGN KEY (CategoryId)
-	REFERENCES Category(Id)
+	REFERENCES Category(Id),
+    CONSTRAINT DF_Book_IsDeleted DEFAULT 0 FOR IsDeleted
 )
 CREATE TABLE [User](
 	Id INT PRIMARY KEY Identity,
-	FullName NVARCHAR(500),
-	PhoneNumber NVARCHAR(13),
+	FullName NVARCHAR(500) Not Null,
+	PhoneNumber NVARCHAR(13) Not Null,
 	RoleId INT,
 	IsDeleted bit,
 	CONSTRAINT FK_User_Role FOREIGN KEY (RoleId)
-	REFERENCES [Role](Id)
+	REFERENCES [Role](Id),
+	CONSTRAINT UQ_User_PhoneNumber UNIQUE (PhoneNumber),
+	CONSTRAINT DF_User_IsDeleted DEFAULT 0 FOR IsDeleted
 )
 CREATE TABLE DownloadedBooks(
 	Id INt PRIMARY KEY IDENTITY,
