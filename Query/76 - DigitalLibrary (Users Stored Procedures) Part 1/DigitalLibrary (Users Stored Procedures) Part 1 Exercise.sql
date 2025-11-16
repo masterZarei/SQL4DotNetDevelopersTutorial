de27f5a63c7758d8586fd9c55e5ef5735b2719aa
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2025-11-16 20:51:07 +0330

    Updating Files.

---
+++ Query/76 - DigitalLibrary (Users Stored Procedures) Part 1/DigitalLibrary (Users Stored Procedures) Part 1 Exercise.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROC sp_CreateUser
+	@FullName NVARCHAR(255),
+	@PhoneNumber NVARCHAR(11),
+	@Password NVARCHAR(MAX),
+	@RoleId INT
+AS
+BEGIN 
+		IF EXISTS(SELECT 1 FROM Users WHERE PhoneNumber = @PhoneNumber) 
+		BEGIN
+			RAISERROR('PhoneNumber already exists',16,1)
+			RETURN;
+		END
+		INSERT INTO Users(FullName, PhoneNumber, [Password], RoleId)
+		VALUES (@FullName, @PhoneNumber, @Password, @RoleId)
+
+		SELECT SCOPE_IDENTITY() AS NewId;
+END
+
+exec sp_CreateUser 'حمید معصومی نژاد', '09366544112', '123', 1
+
+CREATE PROC sp_UpdateUser
+	@Id INT,
+	@FullName NVARCHAR(255),
+	@PhoneNumber NVARCHAR(11),
+	@Password NVARCHAR(MAX),
+	@RoleId INT
+AS
+BEGIN
+	IF EXISTS(SELECT 1 FROM Users WHERE PhoneNumber = @PhoneNumber AND Id <> @Id) 
+		BEGIN
+			RAISERROR('PhoneNumber already exists',16,1)
+			RETURN;
+		END
+	UPDATE Users
+	SET
+		FullName = @FullName,
+		PhoneNumber = @PhoneNumber,
+		RoleId = @RoleId,
+		[Password] = @Password
+	WHERE Id = @Id
+END
+
+exec sp_UpdateUser 1,'حمید معصومی', '09366544112', '123', 1
+
