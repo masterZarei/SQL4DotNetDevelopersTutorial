5214f4db64dd3355fbd82022e9d6bee64fbbebaf
MohammadMahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-06-08 09:30:19 +0330

    Update Company.cs

--- CompanyManagement/CompanyManagement/Models/Company.cs
+++ CompanyManagement/CompanyManagement/Models/Company.cs
@@ -1,4 +1,4 @@
-﻿namespace CompanyManagement.Models
+namespace CompanyManagement.Models
 {
     public class Company
     {
@@ -9,6 +9,6 @@
         public string City { get; set; }
         public string PostalCode { get; set; }
 
-        public virtual List<Employee> Employees { get; set; }
+        public virtual List<Employee>? Employees { get; set; }
     }
 }
